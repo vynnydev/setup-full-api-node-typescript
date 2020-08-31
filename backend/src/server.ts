5860adbc456bfe8d0397b1cb4166160560fc5b5e
vynnydev <vynnyprudencio@gmail.com>
2020-08-31 14:02:35 -0300

    full api setup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,19 @@
 import { Server } from '@overnightjs/core/lib/Server';
 import './util/module-alias';
+import bodyParser from 'body-parser';
 
 export class SetupServer extends Server {
-  
+
+  constructor(private port = 3000) {
+    super()
+  }
+
+  private SetupExpress(): void {
+    //decodificar dados em json
+    this.app.use(bodyParser.json())
+  }
+
+  public init(): void {
+    this.SetupExpress()
+  }
 }
